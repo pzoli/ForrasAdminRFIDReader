54ef97045b5e194b8d45e970bc5f81312bd608de
pzoli <papp.zoltan@infokristaly.hu>
2025-07-27 11:58:41 +0200

    printIPToSerial to print serverip

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@
 #include <ArduinoJson.h>
 JsonDocument doc;
 
-#define RST_PIN         9          // Configurable, see typical pin layout above
-#define SS_PIN          7         // Configurable, see typical pin layout above
-#define BUZZER_PIN      6
+#define RST_PIN 9 // Configurable, see typical pin layout above
+#define SS_PIN 7  // Configurable, see typical pin layout above
+#define BUZZER_PIN 6
 
 #define RGB_R 2
 #define RGB_G 3
@@ -17,7 +17,7 @@ JsonDocument doc;
 #define DEBUG
 #define DHCP
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 
 struct NetConfig
 {
@@ -31,14 +31,25 @@ struct NetConfig
   uint8_t dnsserver[4];
 } conf;
 
+void printIPToSerial(String ipname, IPAddress addr)
+{
+  char ipno[16];
+  sprintf(ipno, "%d.%d.%d.%d", addr[0], addr[1], addr[2], addr[3]);
+  Serial.print(ipname);
+  Serial.println(ipno);
+};
 
-void initEthernet() {
+void initEthernet()
+{
 #ifdef DHCP
-  if (conf.usedhcp == 1) {
+  if (conf.usedhcp == 1)
+  {
     Serial.println(F("Ethernet configure useing DHCP"));
     Ethernet.begin(conf.mac);
     Ethernet.maintain();
-  } else {
+  }
+  else
+  {
 #endif
     Serial.println(F("Ethernet configure using fix IP"));
     Ethernet.begin(conf.mac, conf.ip, conf.dnsserver, conf.gateway, conf.subnet);
@@ -47,19 +58,22 @@ void initEthernet() {
 #endif
 }
 
-String getMACasString(uint8_t* mac) {
+String getMACasString(uint8_t *mac)
+{
   char macno[32];
   sprintf(macno, "%02X-%02X-%02X-%02X-%02X-%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
   return String(macno);
 }
 
-void setColor(int redValue, int greenValue,  int blueValue) {
+void setColor(int redValue, int greenValue, int blueValue)
+{
   analogWrite(RGB_R, redValue);
-  analogWrite(RGB_G,  greenValue);
+  analogWrite(RGB_G, greenValue);
   analogWrite(RGB_B, blueValue);
 }
 
-void setup() {
+void setup()
+{
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
   pinMode(10, OUTPUT);
@@ -70,40 +84,44 @@ void setup() {
   digitalWrite(4, HIGH);
   pinMode(BUZZER_PIN, OUTPUT);
 
-  pinMode(RGB_R,  OUTPUT);              
+  pinMode(RGB_R, OUTPUT);
   pinMode(RGB_G, OUTPUT);
   pinMode(RGB_B, OUTPUT);
 
   setColor(255, 0, 0);
   delay(1000);
-  setColor(0,  255, 0);
+  setColor(0, 255, 0);
   delay(1000);
-  setColor(0, 0, 255); 
+  setColor(0, 0, 255);
   delay(1000);
-  setColor(255, 255, 255); 
+  setColor(255, 255, 255);
 
 #ifdef DEBUG
-  while (!Serial) {
+  while (!Serial)
+  {
     ;
   }
 #endif
   EEPROM_readAnything(1, conf);
   bool configured = String(conf.state).startsWith("CONFIG");
-  if (configured) {
+  if (configured)
+  {
     initEthernet();
-  } 
-  Serial.println(configured?F("Configured"):F("Not confugured yet"));
-  
-  if (configured) {
+  }
+  Serial.println(configured ? F("Configured") : F("Not confugured yet"));
+
+  if (configured)
+  {
     Serial.print(F("reader ip: "));
     Serial.println(Ethernet.localIP());
+    printIPToSerial(F("server ip: "), conf.serverip);
   }
 
   Serial.println("init RFID reader...");
-  mfrc522.PCD_Init();   // Init MFRC522
-  #ifdef DEBUG
-    mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
-  #endif
+  mfrc522.PCD_Init(); // Init MFRC522
+#ifdef DEBUG
+  mfrc522.PCD_DumpVersionToSerial(); // Show details of PCD - MFRC522 Card Reader details
+#endif
   Serial.println("RFID reader initialized.");
   tone(BUZZER_PIN, 1000);
   delay(250);
@@ -116,24 +134,33 @@ bool inJSON = false;
 String webResult = "";
 String command = "";
 
-void loop() {
-  if (webClient.available()) {
+void loop()
+{
+  if (webClient.available())
+  {
     char buff[64];
     int cnt = webClient.readBytes(buff, sizeof(buff));
-    for(int i=0;i<cnt;i++) {
-      if (buff[i] == '{' && !inJSON) {
+    for (int i = 0; i < cnt; i++)
+    {
+      if (buff[i] == '{' && !inJSON)
+      {
         inJSON = true;
         webResult = "";
-      } else if (buff[i] == '}' && inJSON) {
+      }
+      else if (buff[i] == '}' && inJSON)
+      {
         inJSON = false;
         webResult += buff[i];
-        if (webResult.equals(F("{\"RESPONSE\":\"OK\"}"))) {
+        if (webResult.equals(F("{\"RESPONSE\":\"OK\"}")))
+        {
           setColor(0, 255, 0);
           tone(BUZZER_PIN, 1000);
           delay(250);
           noTone(BUZZER_PIN);
           setColor(255, 255, 255);
-        } else {
+        }
+        else
+        {
           setColor(255, 0, 0);
           tone(BUZZER_PIN, 1000);
           delay(250);
@@ -145,7 +172,8 @@ void loop() {
           setColor(255, 255, 255);
         }
       }
-      if (inJSON) {
+      if (inJSON)
+      {
         webResult += buff[i];
       }
     }
@@ -154,37 +182,41 @@ void loop() {
 #endif
   }
 
-  if (!webClient.connected() && dataSent) {
+  if (!webClient.connected() && dataSent)
+  {
     webClient.stop();
     dataSent = false;
   }
-    // Look for new cards
-  if ( mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+  // Look for new cards
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
+  {
     String cardId;
-    for ( uint8_t i = 0; i < 4; i++) {
+    for (uint8_t i = 0; i < 4; i++)
+    {
       byte readCard = mfrc522.uid.uidByte[i];
       char chrHex[2];
       sprintf(chrHex, "%02X", readCard);
       cardId += chrHex;
     }
     mfrc522.PICC_HaltA(); // Stop reading
-    
+
 #ifdef DEBUG
-      Serial.println();
-      Serial.print(F("cardid:"));
-      Serial.println(cardId);
+    Serial.println();
+    Serial.print(F("cardid:"));
+    Serial.println(cardId);
 #endif
-    if (webClient.connect(conf.serverip, 8080)) {
+    if (webClient.connect(conf.serverip, 8080))
+    {
       webClient.print(F("GET "));
       String request = String(F("/forras-admin/rest/createNFCLog?readerid=%RID%&rfid=%CID%&type=RF1"));
-      request.replace(F("%RID%"),getMACasString(conf.mac));
-      request.replace(F("%CID%"),cardId);
+      request.replace(F("%RID%"), getMACasString(conf.mac));
+      request.replace(F("%CID%"), cardId);
 #ifdef DEBUG
-        Serial.println();
-        Serial.print(F("request:"));
-        Serial.println(request);
+      Serial.println();
+      Serial.print(F("request:"));
+      Serial.println(request);
 #endif
-      webClient.print(request);        
+      webClient.print(request);
       webClient.println(F(" HTTP/1.1"));
       webClient.print(F("Host: "));
       String serverIp = doc["serverip"];
@@ -193,7 +225,9 @@ void loop() {
       webClient.println(F("Connection: close"));
       webClient.println();
       dataSent = true;
-    } else {
+    }
+    else
+    {
 #ifdef DEBUG
       Serial.println(F("connection failed"));
       setColor(0, 0, 255);
@@ -202,6 +236,6 @@ void loop() {
       noTone(BUZZER_PIN);
       setColor(255, 255, 255);
 #endif
-    } 
+    }
   }
 }
